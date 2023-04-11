32be191cbe9e6d2466329f7022b76a823a93f8d2
fuvx22 <voxngocphu@gmail.com>
2023-04-11 22:25:50 +0700

    update GUI

--- src/view/books_view.java
+++ src/view/books_view.java
@@ -6,68 +6,159 @@ import javax.swing.JTextField;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Rectangle;
+import java.awt.ScrollPane;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Color;
 
 public class books_view extends JPanel {
-	private JTextField textName;
-	private JTextField textAuthor;
-	private JTextField textQuan;
-	private JTextField textNXB;
+	private JTextField textField;
+	private JTextField authorText;
+	private JTextField dateText;
+	private JTextField nameText;
+	private JTextField quanText;
+	private JTable table;
+	
+	private Color primaryColor = new Color(0, 240, 240);
+	
 
 	/**
 	 * Create the panel.
 	 */
 	public books_view() {
-		setLayout(new BorderLayout(0, 0));
+		setBackground(new Color(0, 128, 128));
+		setLayout(new BorderLayout(0, 20));
+		setBounds(0, 0, 946, 663);
+		setOpaque(true);
 		
-		JPanel inputTab = new JPanel();
-		inputTab.setPreferredSize(new Dimension(WIDTH, 150));
-		inputTab.setSize(WIDTH, 300);
-		add(inputTab, BorderLayout.NORTH);
-		inputTab.setLayout(null);
+		JPanel controlView = new JPanel();
+		controlView.setOpaque(false);
+		controlView.setPreferredSize(new Dimension(1, 120));
+		controlView.setSize(WIDTH, 300);
+		add(controlView, BorderLayout.NORTH);
+		controlView.setLayout(null);
+		
+		JPanel input_tab = new JPanel();
+		input_tab.setOpaque(false);
+		input_tab.setBounds(10, 10, 926, 67);
+		controlView.add(input_tab);
+		input_tab.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Tên sách");
-		lblNewLabel.setBounds(10, 10, 82, 22);
-		inputTab.add(lblNewLabel);
-		
-		textName = new JTextField();
-		textName.setBounds(102, 12, 113, 20);
-		inputTab.add(textName);
-		textName.setColumns(10);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setBounds(67, 15, 89, 16);
+		input_tab.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Tác giả");
-		lblNewLabel_1.setBounds(10, 42, 82, 22);
-		inputTab.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(67, 37, 89, 26);
+		input_tab.add(lblNewLabel_1);
 		
-		textAuthor = new JTextField();
-		textAuthor.setColumns(10);
-		textAuthor.setBounds(102, 44, 113, 20);
-		inputTab.add(textAuthor);
+		authorText = new JTextField();
+		authorText.setBounds(166, 39, 218, 26);
+		authorText.setColumns(10);
+		input_tab.add(authorText);
 		
 		JLabel lblNewLabel_2 = new JLabel("Số lượng");
-		lblNewLabel_2.setBounds(227, 42, 82, 22);
-		inputTab.add(lblNewLabel_2);
-		
-		textQuan = new JTextField();
-		textQuan.setColumns(10);
-		textQuan.setBounds(319, 44, 113, 20);
-		inputTab.add(textQuan);
-		
-		textNXB = new JTextField();
-		textNXB.setColumns(10);
-		textNXB.setBounds(319, 12, 113, 20);
-		inputTab.add(textNXB);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(446, 18, 89, 13);
+		input_tab.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Ngày xuất bản");
-		lblNewLabel_2_1.setBounds(227, 10, 82, 22);
-		inputTab.add(lblNewLabel_2_1);
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2_1.setBounds(446, 41, 89, 26);
+		input_tab.add(lblNewLabel_2_1);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 88, 723, 62);
-		inputTab.add(panel);
+		dateText = new JTextField();
+		dateText.setBounds(545, 39, 218, 26);
+		dateText.setColumns(10);
+		input_tab.add(dateText);
+		
+		nameText = new JTextField();
+		nameText.setColumns(10);
+		nameText.setBounds(166, 10, 218, 26);
+		input_tab.add(nameText);
+		
+		quanText = new JTextField();
+		quanText.setColumns(10);
+		quanText.setBounds(545, 10, 218, 26);
+		input_tab.add(quanText);
+		
+		JPanel find_tab = new JPanel();
+		find_tab.setOpaque(false);
+		find_tab.setBounds(370, 87, 566, 33);
+		controlView.add(find_tab);
+		find_tab.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setBounds(0, 1, 446, 33);
+		textField.setPreferredSize(new Dimension(200, 22));
+		textField.setColumns(10);
+		find_tab.add(textField);
+		
+		JButton find_btn = new JButton("Tìm kiếm");
+		find_btn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		find_btn.setPreferredSize(new Dimension(100, 30));
+		find_btn.setBounds(new Rectangle(456, 0, 100, 33));
+		find_tab.add(find_btn);
+		
+		JButton edit_btn = new JButton("Sửa");
+		edit_btn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		edit_btn.setPreferredSize(new Dimension(100, 30));
+		edit_btn.setBounds(new Rectangle(220, 5, 90, 20));
+		edit_btn.setBounds(239, 87, 105, 33);
+		controlView.add(edit_btn);
+		
+		JButton add_btn = new JButton("Thêm");
+		add_btn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		add_btn.setPreferredSize(new Dimension(100, 30));
+		add_btn.setBounds(new Rectangle(30, 5, 90, 20));
+		add_btn.setBounds(10, 87, 105, 33);
+		controlView.add(add_btn);
+		
+		JButton del_btn = new JButton("Xóa");
+		del_btn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		del_btn.setPreferredSize(new Dimension(100, 30));
+		del_btn.setBounds(new Rectangle(125, 5, 90, 20));
+		del_btn.setBounds(125, 87, 104, 33);
+		controlView.add(del_btn);
 		
 		JPanel dataView = new JPanel();
+		dataView.setOpaque(false);
 		add(dataView, BorderLayout.CENTER);
+		dataView.setLayout(new BorderLayout(0, 0));
+		
+		Object data[][]={ {"Sv01","Nguyen Van An","7.5"}, 
+				{"Sv02","Le THi THuong","8.0"}, 
+				{"Sv03","Hoang Ha","6.5"}};
+				String column[]={"ID","NAME","MARK"}; 
 
+		
+		table = new JTable(data, column);
+		table.setBackground(primaryColor);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.getViewport().setBackground(primaryColor);
+		scrollPane.setBackground(new Color(0, 159, 159));
+		
+
+		DefaultTableModel model = new DefaultTableModel(data,column);	
+		model.addRow(new Object[]{"Sv04","Trinh Phu","10"});
+		table.setModel(model);
+		
+		dataView.add(scrollPane, BorderLayout.NORTH);
+		
+		
 	}
 }
--- src/view/home_view.java
+++ src/view/home_view.java
@@ -27,6 +27,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import javax.swing.SwingConstants;
 
 public class home_view extends JFrame {
 
@@ -40,9 +41,8 @@ public class home_view extends JFrame {
 	private JPanel back_panel;
 	private JPanel stat_panel;
 	private JPanel lib_panel;
-	private CardLayout card;
-	public int x = 1;
 	private JLabel home_btn;
+	private CardLayout card;
 
 	/**
 	 * Launch the application.
@@ -77,67 +77,75 @@ public class home_view extends JFrame {
 		JPanel sideMenu = new JPanel();
 		sideMenu.setBounds(0, 0, 141, 663);
 		sideMenu.setMinimumSize(new Dimension(300, 10));
-		sideMenu.setBackground(new Color(128, 128, 255));
+		sideMenu.setBackground(new Color(0, 128, 128));
 		contentPane.add(sideMenu);
 		
 		sideMenu.setLayout(null);
 		
 		home_btn = new JLabel("Trang chủ");
+		home_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		home_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		home_btn.setForeground(new Color(255, 255, 255));
-		home_btn.setBounds(10, 11, 131, 35);
+		home_btn.setBounds(0, 11, 141, 35);
 		home_btn.setName("home_btn");
 		sideMenu.add(home_btn);
 		
 		
 		
 		JLabel book_btn = new JLabel("Quản lý sách");
+		book_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		book_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		book_btn.setForeground(new Color(255, 255, 255));
-		book_btn.setBounds(10, 57, 131, 35);
+		book_btn.setBounds(0, 57, 141, 35);
 		book_btn.setName("book_btn");
 		sideMenu.add(book_btn);
 		
 		JLabel reader_btn = new JLabel("Quản lý độc giả");
+		reader_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		reader_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		reader_btn.setForeground(new Color(255, 255, 255));
-		reader_btn.setBounds(10, 103, 131, 35);
+		reader_btn.setBounds(0, 103, 141, 35);
 		reader_btn.setName("reader_btn");
 		sideMenu.add(reader_btn);
 		
 		JLabel loan_btn = new JLabel("Quản lý mượn sách");
+		loan_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		loan_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		loan_btn.setForeground(new Color(255, 255, 255));
-		loan_btn.setBounds(10, 149, 131, 35);
+		loan_btn.setBounds(0, 149, 141, 35);
 		loan_btn.setName("loan_btn");
 		sideMenu.add(loan_btn);
 		
 		JLabel back_btn = new JLabel("Quản lý trả sách");
+		back_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		back_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		back_btn.setForeground(new Color(255, 255, 255));
-		back_btn.setBounds(10, 195, 131, 35);
+		back_btn.setBounds(0, 195, 141, 35);
 		back_btn.setName("back_btn");
 		sideMenu.add(back_btn);
 		
 		JLabel stat_btn = new JLabel("Thống kê");
+		stat_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		stat_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		stat_btn.setForeground(new Color(255, 255, 255));
-		stat_btn.setBounds(10, 241, 131, 35);
+		stat_btn.setBounds(0, 241, 141, 35);
 		stat_btn.setName("stat_btn");
 		sideMenu.add(stat_btn);
 		
 		JLabel logout_btn = new JLabel("Đăng xuất");
+		logout_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		logout_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		logout_btn.setForeground(new Color(255, 255, 255));
-		logout_btn.setBounds(10, 615, 131, 35);
+		logout_btn.setBounds(0, 615, 141, 35);
 		logout_btn.setName("logout_btn");
 		sideMenu.add(logout_btn);
 		
 		JLabel lib_btn = new JLabel("Quản lý thủ thư");
+		lib_btn.setHorizontalAlignment(SwingConstants.LEFT);
 		lib_btn.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lib_btn.setForeground(new Color(255, 255, 255));
-		lib_btn.setBounds(10, 569, 131, 35);
+		lib_btn.setBounds(0, 569, 141, 35);
 		lib_btn.setName("lib_btn");
 		sideMenu.add(lib_btn);
 		
@@ -149,8 +157,9 @@ public class home_view extends JFrame {
 		card = new CardLayout(0, 0);
 		mainPanel.setLayout(card);
 		
-		home_panel = new JPanel();
-		home_panel.setBackground(new Color(128, 255, 128));
+		home_panel = new books_view();
+		home_panel.setBackground(new Color(0, 159, 159));
+//		home_panel.setBackground(new Color(128, 255, 128));
 		mainPanel.add(home_panel, "home_btn");
 		
 		book_panel = new JPanel();
@@ -176,16 +185,6 @@ public class home_view extends JFrame {
 		lib_panel = new JPanel();
 		lib_panel.setBackground(new Color(0, 128, 128));
 		mainPanel.add(lib_panel, "lib_btn");
-		
-		panelList = new ArrayList<>();
-		
-		panelList.add(home_panel);
-		panelList.add(book_panel);
-		panelList.add(reader_panel);
-		panelList.add(loan_panel);
-		panelList.add(back_panel);
-		panelList.add(stat_panel);
-		panelList.add(lib_panel);
 		
 		
 		homeView_controller control = new homeView_controller(this);
